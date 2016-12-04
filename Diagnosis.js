559a996d7c1496f0d1512f4ad0c499212bfbb246
magnimm <manammagnumm@gmail.com>
2016-12-04 12:59:10 +0700

    555

--- Diagnosis.js
+++ Diagnosis.js
@@ -15,6 +15,14 @@ import {
 } from 'react-native';
 
 class Diagnosis extends Component {
+  constructor(props){
+    super(props);
+    this.state = {HN:"",Currentdate:"",Symptom:"",Disagnosis Disease:"",Treatment:"",Remark:""};
+    this.database = firebase.database();
+    this.writeDB = this.writeDB.bind(this);
+  }
+
+
   render() {
     return (
       <View style={styles.container}>
@@ -22,12 +30,12 @@ class Diagnosis extends Component {
           <Text style={styles.welcome}>Diagnosis</Text>
         </View>
         <View style={styles.Vmiddle}>
-          <TextInput keyboardType='numeric' placeholder="HN" style={styles.inputtext}/>
-          <TextInput keyboardType='phone-pad' placeholder="Current Date" style={styles.inputtext}/>
-          <TextInput keyboardType='default' placeholder="Symptom" style={styles.inputtext}/>
-          <TextInput keyboardType='default' placeholder="Diagnosis Disease" style={styles.inputtext}/>
-          <TextInput keyboardType='default' placeholder="Treatment" style={styles.inputtext}/>
-          <TextInput keyboardType='default' placeholder="Remark" style={styles.inputtext}/>
+        <TextInput value={this.state.HN} onChangeText={(HN)=>this.setState({HN})} keyboardType='numeric' placeholder="HN" style={styles.inputtext}/>
+        <TextInput value={this.state.Currentdate} onChangeText={(Currentdate)=>this.setState({Currentdate})} keyboardType='default' placeholder="Currentdate" style={styles.inputtext}/>
+        <TextInput value={this.state.Symptom} onChangeText={(Symptom)=>this.setState({Symptom})} keyboardType='default' placeholder="Symptom" style={styles.inputtext}/>
+        <TextInput value={this.state.Diagnosis} onChangeText={(Diagnosis Disease)=>this.setState({Diagnosis Disease})} keyboardType='default' placeholder="Diagnosis Disease" style={styles.inputtext}/>
+        <TextInput value={this.state.Treatment} onChangeText={(Treatment)=>this.setState({Treatment})} keyboardType='default' placeholder="Treatment" style={styles.inputtext}/>
+        <TextInput value={this.state.Remark} onChangeText={(Remark)=>this.setState({Remark})} keyboardType='default' placeholder="Remark" style={styles.inputtext}/>
         </View>
         <View style={styles.Vbottom}>
         <View style={styles.Vbottom}>
